565457ff20437c91723e0694c663ec0b047e2ecc
defrm <efremov.dmitry.gl@gmail.com>
2024-02-13 15:24:23 +0300

    Сделать липким список услуг (класс sticky)

--- php/partials/services-list.php
+++ php/partials/services-list.php
@@ -1,4 +1,4 @@
-<div class="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+<div class="sticky nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
     <ul class="services__list">
         <li> <button class="fs-4 w-100 text-start nav-link active" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">Клининг</button></li>
         <li> <button class="fs-4 w-100 text-start nav-link" id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false">Герметизация швов</button> </li>
